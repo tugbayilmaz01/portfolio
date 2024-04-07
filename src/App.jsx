49c9b9c8d620d0c2d023f8436972112a605c1eec
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-04-07 17:01:01 +0300

    creating animated sidebar & adding it to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar />
       </section>
-      <section>Parallax</section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
+      <section id="Portfolio">Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/*Sidebar*/}
+      <Sidebar />
       <div className="wrapper">
         <span>Tuğba Yılmaz</span>
         <div className="social">
