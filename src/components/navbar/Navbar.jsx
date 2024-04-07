a1508fddced6bdd26c78675983cd5978f1c5e0cf
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-04-07 14:48:51 +0300

    navbar created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/*Sidebar*/}
+      <div className="wrapper">
+        <span>Tuğba Yılmaz</span>
+        <div className="social">
+          <a href="#">
+            {" "}
+            <img className="png" src="/linkedin.png"></img>{" "}
+          </a>
+          <a href="#">
+            {" "}
+            <img className="png" src="/github.png"></img>{" "}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
