993bfbb0a7a147d12ae9fe7f8964dc2030cab958
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-04-08 16:45:12 +0300

    animations added to sidebar & social media links added to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,18 @@
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
-
+import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <div className="navbar">
       <Sidebar />
       <div className="wrapper">
-        <span>Tuğba Yılmaz</span>
+        <div></div>
         <div className="social">
-          <a href="#">
+          <a href="https://www.linkedin.com/in/tugbayilmaz01/">
             {" "}
             <img className="png" src="/linkedin.png"></img>{" "}
           </a>
-          <a href="#">
+          <a href="https://github.com/tugbayilmaz01">
             {" "}
             <img className="png" src="/github.png"></img>{" "}
           </a>
