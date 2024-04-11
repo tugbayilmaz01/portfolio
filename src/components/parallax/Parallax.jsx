fb66df2c01914f38f6d5821363247ff8702295ae
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-04-11 22:50:21 +0300

    background color of transition pages changed to the same color

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -18,10 +18,7 @@ const Parallax = ({ type }) => {
       className="parallax"
       ref={ref}
       style={{
-        background:
-          type === "services"
-            ? "linear-gradient(180deg, #111132, #0c0c1d)"
-            : "linear-gradient(180deg, #111132, #505064)",
+        background: "linear-gradient(180deg, #111132, #0c0c1d)",
       }}
     >
       <motion.h1 style={{ y: yText }}>
