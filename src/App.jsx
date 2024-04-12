a416aa6c4690129928cd9657d9f70b29675f4cbf
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-04-12 15:18:34 +0300

    projects section created with animations & progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss";
 import Developer from "./components/developer/Developer";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
 
 const App = () => {
@@ -20,9 +21,7 @@ const App = () => {
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
+      <Portfolio />
       <section id="Contact">Contact</section>
     </div>
   );
